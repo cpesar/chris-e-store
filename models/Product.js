d71f4499f3d7effa337578676ef8ea39053a05bd
cpesar <77510555+cpesar@users.noreply.github.com>
2021-06-12 16:37:25 -0600

    completed models and joins

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,7 @@
 // import important parts of sequelize library
+const { validate } = require('@babel/types');
 const { Model, DataTypes } = require('sequelize');
+const { values } = require('sequelize/types/lib/operators');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -10,6 +12,48 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      //HOW TO VALIDATE IF THIS IS A DECIMAL
+      validate: {
+        isDecimal: true
+      }
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      //Set a default value of 10
+      defaultValue: 10,
+
+      //Validates that the value is numeric
+      validate:{
+        isNumeric: true
+      }
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      //References the category model's id
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
+
   },
   {
     sequelize,
